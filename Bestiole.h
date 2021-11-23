48f1a7f249f0bbfdc1fe7bdf00e3bdfe03ebf9a8
camillefm <camille.francois-martin@imt-atlantique.net>
2021-11-23 17:16:32 +0100

    ajout attribut et méthode dans bestiole.h

--- Bestiole.h
+++ Bestiole.h
@@ -3,6 +3,9 @@
 
 
 #include "UImg.h"
+#include "Comportement.h"
+#include "Accessoire.h"
+#include "Capteur.h"
 
 #include <iostream>
 
@@ -22,6 +25,7 @@ private :
 
    static int              next;
 
+
 private :
    int               identite;
    int               x, y;
@@ -30,6 +34,19 @@ private :
    double            vitesse;
 
    T               * couleur;
+
+      //new code :
+   Comportement      comportement;
+
+   listCapteurs :    listCapteur
+   listAccessoires:  listAccesoires
+   int               dieProbabilty
+   int               camouflage
+
+   int :             ageLimite
+   int :             age
+
+
 
 private :
    void bouge( int xLim, int yLim );

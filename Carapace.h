d32b122b1237812fa45e0605cc23565b89ba4646
camillefm <camille.francois-martin@imt-atlantique.net>
2021-11-23 17:37:32 +0100

    [add] ajout des attribut et des méthodes dans le .h des différents accessoires

---
+++ Carapace.h
@@ -0,0 +1,20 @@
+#ifndef _CARAPACE_H_
+#define _CARAPACE_H_
+#include <iostream>
+#include <CImg.h>
+#include "Bestiole.h"
+
+Class Carapace : public Accessoire{
+
+
+	public : 
+		void modifieCaracteristique(int value,Bestiole bestiole);
+
+}
+
+
+
+
+
+
+#endif
---
+++ Nageoire.h
@@ -0,0 +1,17 @@
+#ifndef _NAGEOIRE_H_
+#define _NAGEOIRE_H_
+#include <iostream>
+#include <CImg.h>
+#include "Bestiole.h"
+
+Class Nageoire : public Accessoire{
+
+	public : 
+		void modifieCaracteristique(int value,Bestiole bestiole);
+
+}
+
+
+
+
+#endif
